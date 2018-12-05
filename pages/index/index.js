985bf5b7b8623d97aa3b0ee9a9bcd799f754719e
sunlihan <2466492654@qq.com>
2018-12-05 11:28:29 +0800

    修改index.wxml轮播和index.wxss和jiuyesearch.wxml和jiuyesearch.wxss

--- pages/index/index.js
+++ pages/index/index.js
@@ -85,9 +85,12 @@ Page({
     zhengceData:[],
     user:'',
     imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      '../img/banmer-1.jpg',
+      '../img/banmer-2.jpg',
+      '../img/banmer-3.jpg',
+      '../img/banmer-4.jpg',
+      '../img/banmer-5.jpg',
+      '../img/banmer-6.jpg'
     ],
     indicatorDots: true,
     autoplay: true,
