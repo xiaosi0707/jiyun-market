0c44ccfb97c606b74edfc8f27cc82e6df43a8e1c
author-hao <1357569778@qq.com>
2018-12-05 09:50:29 +0800

    登录页修改了

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,73 +1,119 @@
-//logs.js
-import {HTTP} from '../../utils/http.js'
-let http=new HTTP()
+//登录页
 
 Page({
   data: {
-    user:'',
-    tel: '',
-    pass:''
+    userName: '', // 获取用户姓名值
+    userNum: '', // 获取用户手机号 
+    userPwd: '', // 获取用户密码
+    Border: '', // 姓名获取焦点显示下边框
+    Num: '', // 手机号获取焦点显示下边框
+    Pwd: '', //  密码获取焦点显示下边框
+    logs: '' // 提示
   },
-  name:function(e){
+  onfocusName: function (e) { // 姓名框获取焦点
     this.setData({
-      user:e.detail.value
+      logs: '',
+      Border: 'transition:all .3s ease-in-out; width: 100%;'
     })
   },
-  tel:function(e){
+  onblurName: function (e) { // 姓名框失去焦点
+    let user = e.detail.value // 获取姓名的值
     this.setData({
-      tel: e.detail.value
+      userName: user,
+      Border: 'transition:all .3s ease-in-out; width: 0;'
     })
-    // console.log(this.data.tel)
   },
-  pass:function(e){
+  onfocusNum: function () { // 手机号获取焦点
     this.setData({
-      pass: e.detail.value
+      logs: '',
+      Num: 'transition:all .3s ease-in-out; width: 100%;'
     })
-    // console.log(this.data.pass)  
   },
-  login:function(){
-    let that = this
-    wx.setStorageSync("user",that.data.user)
-    if (that.data.user || that.data.tel || that.data.pass){
-      http.request({
-        url: 'api/login',
-        method: "POST",
-        data: {
-          "username": that.data.user,  //陈子安
-          "tel": that.data.tel,        //15010585137
-          "password": that.data.pass  //admin
-          // "username": "陈子安",  //陈子安
-          // "tel": "15010585137",        //15010585137
-          // "password": "admin"   //admin
-
-
+  onblurNum: function (e) { // 手机号失去焦点
+    let number = e.detail.value
+    this.setData({
+      userNum: number,
+      Num: 'transition:all .3s ease-in-out; width: 0;'
+    })
+  },
+  onfocusPwd: function () { // 密码获取焦点
+    this.setData({
+      logs: '',
+      Pwd: 'transition:all .3s ease-in-out; width: 100%;'
+    })
+  },
+  onblurPwd: function (e) { // 密码失去焦点
+    let pass = e.detail.value
+    this.setData({
+      userPwd: pass,
+      Pwd: 'transition:all .3s ease-in-out; width: 0;'
+    })
+  },
+  onlogin: function () { // 登录按钮操作
+    let user = this.data.userName
+    let num = this.data.userNum
+    let pass = this.data.userPwd
+    var that = this
+    if (!user) {
+      this.setData({
+        logs: '请输入姓名'
+      })
+    } else if (!num) {
+      this.setData({
+        logs: '请输入手机号'
+      })
+    } else if (!(/^1(3|4|5|6|7|8|9)\d{9}$/).test(num)) {
+      this.setData({
+        logs: '请输入正确手机号'
+      })
+    } else if (!pass) {
+      this.setData({
+        logs: '请输入密码'
+      })
+    } else {
+      wx.request({
+        method: 'post',
+        url: 'http://jy.haoyunyun.cn/api/login',
+        data: { 'username': user, 'tel': num, 'password': pass },
+        header: {
+          "content-type": "application/json"
         },
-        success: (res) => {
-          console.log(res)
-          if(res.status===200){
-            wx.showToast({
-              title: '成功',
-              success:function(){
-                wx.navigateTo({
-                  url: '../index/index',
-                })
-              }
-            })
-          }else{
-            wx.showModal({
-              title: '错误提示',
-              content: res.message,
+        dataType: 'json',
+        responseType: 'text',
+        success: function (res) {
+          console.log(res.data)
+          if (res.statusCode == 200) {
+            let { data } = res
+            if (data.status === 200) {
+              wx.showToast({
+                title: '登录成功',
+                success: function () {
+                  setTimeout(()=> {
+                    wx.redirectTo({ // 路由跳转
+                      url: "/pages/index/index" // 登陆成功后跳转到首页
+                    })
+                  }, 500)
+                }
+              })
+              wx.setStorage({
+                key: 'user',
+                data: user,
+              })
+             
+            } else {
+              that.setData({
+                logs: data.message
+              })
+            }
+
+          } else {
+            that.setData({
+              logs:'登录失败'
             })
           }
-        }
-      })
-    }else{
-      wx.showModal({
-        title: '错误提示',
-        content: "请输入信息",
+          
+        },
       })
     }
-  },
-  onLoad: function () {
-   }
+  }
 })
