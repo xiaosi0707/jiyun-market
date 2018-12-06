028a509fb94a376f4b2084ab86e85d0e20afe23f
于明鑫 <1930667309@qq.com>
2018-12-06 15:19:14 +0800

    学生统计

--- pages/studentsearch/studentsearch.js
+++ pages/studentsearch/studentsearch.js
@@ -20,18 +20,36 @@ Page({
   },
   onSearchList () { // 搜索
     let user = this.data.userName
-    var indata = []
+    var indata = [];  //便利后的数据
     http.getSearchData(user).then(res=> {
-      console.log(res.data)
-      indata = res.data
-    })
-    http.getSearchList(user).then(res => {
-      console.log(res.data)
+      let data = res.data;  //学生信息
+      http.getSearchList(user).then(res => {
+        let data1 = res.data;  //班级信息
+        http.getSearchPrice(user).then(res => {
+          let data2 = res.data; //缴费信息
+          for(let i = 0;data[i]!=undefined;i++){
+            let datas = {
+              student_inform:[],
+              The_class_inform:[],
+              Payment_inform:[]
+            };
+            datas.student_inform.push(data[i]);
 
-      for (var i = 0; res.data[i]!=undefined; i++) {
-       
-      }
-    
+            for(let j = 0;data1[j]!=undefined;j++){
+              if(data[i].StudentIdCard == data1[j].StudentIdCard){
+                datas.The_class_inform.push(data1[j]);
+              }
+            }
+            for(let k = 0;data2[k]!=undefined;k++){
+              if (data[i].StudentIdCard == data2[k].StudentIdCard) {
+                datas.Payment_inform.push(data2[k]);
+              }
+            }
+            indata.push(datas);
+          }
+          console.log(indata);
+        })
+      })
     })
   }
 })
