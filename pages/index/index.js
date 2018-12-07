af1761114cb0c277e057dfccb14400aefb6c7f5a
author-hao <1357569778@qq.com>
2018-12-07 11:42:10 +0800

    修复bug

--- pages/index/index.js
+++ pages/index/index.js
@@ -156,8 +156,6 @@ Page({
   * 生命周期函数--监听页面加载
   */
   onLoad: function (options) {
-    getCurrentPages()[0].title = '哈哈'
-    console.log(getCurrentPages())
     var user = wx.getStorageSync("user")
     this.setData({
       title: options.title,
